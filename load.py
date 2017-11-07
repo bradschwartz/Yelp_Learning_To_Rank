b9583dd82965b4667101c8e37daec2f73af7741e
Brad Schwartz <baschwa@umich.edu>
2017-11-07 10:49:02 -0500

    initialized

---
+++ load.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import sys
+import json
+
+def main():
+	file_name = sys.argv[1]
+	with open(file_name) as file:
+		data = [json.load]
+
+
+
+if __name__ == "__main__":
+	main()
