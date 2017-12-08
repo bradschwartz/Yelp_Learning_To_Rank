80b8a00b78b6401da5bed21e71a6a7517f1ad980
Brad Schwartz <baschwa@umich.edu>
2017-12-07 21:14:43 -0500

    features

--- code/main.py
+++ code/main.py
@@ -1,25 +1,49 @@
 #!/usr/bin/env python3
 import pandas as pd
+import numpy as np
 import load
 from sklearn.pipeline import Pipeline
-from sklearn.model_selection import cross_val_score
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, HashingVectorizer
+from sklearn.model_selection import cross_val_score, GridSearchCV
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, HashingVectorizer, TfidfVectorizer
 from sklearn.linear_model import SGDClassifier
+import pdb
 
 def main():
     prefix = '../playdata/'
-    files = ['business.json', 'checkin.json', 'review.json', 'tip.json']#, 'user.json'] # 'photos.json'
-    df = load.load_data(prefix + files[0])
-    business = df.copy()
 
-    for file in files[1:]:
-        file_df = load.load_data(prefix + file)
-        # Dropping stars since this is how we will judge our accuracy
-        if file_df.get('stars') is not None:
-            file_df.drop('stars', axis=1, inplace=True)
-        df = df.merge(file_df, on='business_id', how='outer')
+    files = 'business.json'
+    business_df = load.load_data(prefix + files)
+    stars = business_df.stars
+    business_df = business_df.drop('stars', axis=1)
+    parameters = {'penalty': ('l2', 'elasticnet'), 'alpha': (0.00001, 0.000001)}
+    sgd = SGDClassifier(max_iter=10000)
+    
 
-    df.fillna(0, inplace=True)
+    vect = TfidfVectorizer()
+    clf = GridSearchCV(sgd, parameters, verbose=1)
+
+
+    df = pd.DataFrame()
+    df['text'] = []
+    for column in business_df.columns:
+        df['text'] = df['text'].map(str) + ' ' + business_df[column].map(str)
+    vectorized = vect.fit_transform(df.text)
+    # pdb.set_trace()
+    clf.fit(vectorized, stars.as_matrix())
+
+
+    # files = ['business.json', 'checkin.json', 'review.json', 'tip.json']#, 'user.json'] # 'photos.json'
+    # df = load.load_data(prefix + files[0])
+    # business = df.copy()
+
+    # for file in files[1:]:
+    #     file_df = load.load_data(prefix + file)
+    #     # Dropping stars since this is how we will judge our accuracy
+    #     if file_df.get('stars') is not None:
+    #         file_df.drop('stars', axis=1, inplace=True)
+    #     df = df.merge(file_df, on='business_id', how='outer')
+
+    # df.fillna(0, inplace=True)
     # at this point, have df of all information about a business. Now, lets start doing ML!
 
     # pipe = Pipeline([('vect', CountVectorizer()),
